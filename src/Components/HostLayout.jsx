d3aa7bba3ce53aca78e2bc1648e9ec0ad247f115
Admin <shivendrawk@gmail.com>
2024-07-25 16:52:36 +0530

    add vans page

--- src/Components/HostLayout.jsx
+++ src/Components/HostLayout.jsx
@@ -5,7 +5,7 @@ export default function HostLayout(){
     return (
         <>
          <nav className='h-10 flex mb-5 md:gap-4 px-[10px] gap-2 items-center justify-start' >
-            <NavLink to="/host" end className="hover:font-bold font-[500]" style={({isActive})=>{
+            <NavLink to="/host" end className="hover:font-bold " style={({isActive})=>{
                 return { fontWeight : isActive ? '700' : ''}
             }}>Dashboard</NavLink>
             <NavLink to="/host/income" className="hover:font-semibold" style={({isActive})=>{
