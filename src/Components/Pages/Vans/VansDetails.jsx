9d74adcdd1665e0fd8b24c31586ac04672dc61bd
Admin <shivendrawk@gmail.com>
2024-07-25 10:46:04 +0530

    details page fix

--- src/Components/Pages/Vans/VansDetails.jsx
+++ src/Components/Pages/Vans/VansDetails.jsx
@@ -1,43 +1,44 @@
 import { useEffect, useState } from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
-export default function VansDetails(){
+export default function VansDetails() {
     let van = useParams();
-    
+
     // console.log(van.id);
     let [specificVan, setSpecificVan] = useState({});
     let [Loading, setLoading] = useState(true);
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`/api/vans/${van.id}`)
-        .then((data)=>{
-            if(!data.ok) throw new Error("Somthing went wrong");
-            else return data.json();
-        })
-        .then((value)=>{
-            // if(!Array.isArray(value.vans)) throw new Error("Response is not an Array");
-            setSpecificVan(value.vans);
-            setLoading(false);
-            // console.log(value.vans)
-        })
-    }, [] ) //If we dont give this empty array then useEffect will call the callback again and again 
+            .then((data) => {
+                if (!data.ok) throw new Error("Somthing went wrong");
+                else return data.json();
+            })
+            .then((value) => {
+                // if(!Array.isArray(value.vans)) throw new Error("Response is not an Array");
+                setSpecificVan(value.vans);
+                setLoading(false);
+                // console.log(value.vans)
+            })
+    }, []) //If we dont give this empty array then useEffect will call the callback again and again 
 
     // console.log(specificVan)
     return (
         <>
-         { Loading ? <h1 className='text-4xl font-semibold absolute top-[50%] -translate-y-[50%] left-[50%] -translate-x-[50%] '>Loading...</h1> : <div className='w-fit my-10'>
-            <img className='w-96 mx-auto rounded-md' src={specificVan.imageUrl} alt="van picture" />
-            <div className='mx-auto w-11/12'>
-                <h2 className='text-3xl font-semibold mx-auto my-4 text-center'>{specificVan.name}</h2>
-                <p className='my-2'>{specificVan.description}</p>
-                <span className='text-xl font-semibold my-4 mr-3'>${specificVan.price}/day</span>
-                <span className={`inline-flex my-1 items-center justify-center px-4 py-1 rounded-md text-white ${specificVan.type == 'simple' ? 'bg-[#b43333]' : (specificVan.type == 'luxury' ? 'bg-[#0c0702]' : 'bg-[rgb(4,80,35)]')} w-fit`}>{specificVan.type}</span>
-                <button className={`my-2 justify-center px-4 py-2 rounded-md text-white bg-[#ff3636] w-full active:bg-black `}>Rent this Van</button>
-            </div>
-            
-        </div>}
-        
+            {Loading ? <h1 className='text-4xl font-semibold absolute top-[50%] -translate-y-[50%] left-[50%] -translate-x-[50%] '>Loading...</h1> : <div className='w-fit my-10'>
+                <img className='w-96 mx-auto rounded-md' src={specificVan.imageUrl} alt="van picture" />
+                <div className='mx-auto w-11/12'>
+                    <span className={`inline-flex my-1 items-center justify-center px-4 py-1 rounded-md text-white ${specificVan.type == 'simple' ? 'bg-[#b43333]' : (specificVan.type == 'luxury' ? 'bg-[#0c0702]' : 'bg-[rgb(4,80,35)]')} w-fit`}>{specificVan.type}</span>
+                    <h2 className='text-3xl font-semibold mx-auto my-4'>{specificVan.name}</h2>
+
+                    <span className='text-xl font-semibold my-4 mr-3'>${specificVan.price}/day</span>
+                    <p className='my-2'>{specificVan.description}</p>
+                    <button className={`my-2 justify-center px-4 py-2 rounded-md text-white bg-[#ff3636] w-full active:bg-black `}>Rent this Van</button>
+                </div>
+
+            </div>}
+
         </>
-       
+
     )
 }
 
