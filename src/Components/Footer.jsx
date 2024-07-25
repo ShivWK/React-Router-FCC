0edbb664eff8de48556c9c458ce0d2e2f628d69b
Admin <shivendrawk@gmail.com>
2024-07-25 13:32:24 +0530

    edit footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer(){
     return (
-        <footer className='h-9 text-white bg-black fixed bottom-0  w-full flex items-center justify-center'>
+        <footer className='h-9 text-white bg-black fixed bottom-0  w-screen flex items-center justify-center'>
             &#169; 2022 #VANLIFE
         </footer>
     )
--- src/Components/HostLayout.jsx
+++ src/Components/HostLayout.jsx
@@ -5,7 +5,7 @@ export default function HostLayout(){
     return (
         <>
          <nav className='h-10 flex md:gap-4 px-[10px] gap-2 items-center justify-start' >
-            <NavLink to="/host" className="hover:font-semibold" style={({isActive})=>{
+            <NavLink to="/host" end className="hover:font-semibold" style={({isActive})=>{
                 return { fontWeight : isActive ? '700' : ''}
             }}>Dashboard</NavLink>
             <NavLink to="/host/income" className="hover:font-semibold" style={({isActive})=>{
