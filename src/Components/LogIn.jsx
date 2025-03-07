80b15ee58b832b078979cf0032ca7e14a4e574f3
Admin <shivendrawk@gmail.com>
2025-03-07 13:32:02 +0530

    implemented defered data UI without using defer() as of router V7.2.0

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -18,46 +18,10 @@ export async function loginAction({ request }) {
 }
 
 export default function LogIn() {
-    // const [loginFormData, setLoginFormData] = useState({email : '', password : ''});
-    // const [status, setStatus] = useState("idle");
-    // const [error, setError] = useState(null);
     const navigate = useNavigation();
     const error = useActionData();
     const isSubmitting = navigate.state === "submitting";
 
-
-    // function handleSubmit(e) {
-    //     e.preventDefault();
-    //     setStatus("submitting");
-    //     setError(null);
-
-    //     navigate("/host", {replace : true})
-
-    //     (async () => {
-    //         try {
-    //             let response = await loginUser(loginFormData);
-    //             console.log(response);
-    //         } 
-    //         catch(error) {
-    //             setError(error.message);
-    //         }
-    //         finally {
-    //             setStatus("idel");
-    //         }
-    //     })();
-    // }
-     
-    // function handleChange(e) {
-    //     const {name, value} = e.target;
-
-    //     setLoginFormData(prev => {
-    //         return {
-    //             ...prev,
-    //             [name] : value
-    //         }
-    //     })
-    // }
-    
 
     return <div className="login-container">
         <h1 className='font-bold text-2xl'>LogIn to your account</h1>
--- src/Components/Pages/Vans/Van.jsx
+++ src/Components/Pages/Vans/Van.jsx
@@ -1,31 +1,31 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { Link, useSearchParams, NavLink, useLoaderData } from 'react-router-dom';
+import React, { useEffect, useMemo, useState, Suspense } from 'react';
+import { Link, useSearchParams, NavLink, useLoaderData , Await} from 'react-router-dom';
+// import { defer } from 'react-router-dom/server';
 import { requiredAuth } from '../../../util';
 
 export async function loader() {
     await requiredAuth();
-    let responce = await fetch('/api/vans');
-    let data = await responce.json();
+    // let responce = await fetch('/api/vans');
+    // let data = await responce.json();
+    // return data;
 
-    return data;
+    let response = fetch('/api/vans');
+    return { vans : response.then(data => data.json()) }
 }
 
 export default function Van() {
 
-    // let [vanData, setVanData] = useState([]);
-    // let [loading, setLoading] = useState(true)
-    let [searchParam, setSearchParams] = useSearchParams();
-
-    let vanData = useLoaderData().vans;
+    let [searchParams, setSearchParams] = useSearchParams();
+    let vanDataPromise = useLoaderData();
 
     // console.log(searchParam)
     // console.log(searchParam.toString());
 
     // will be printed for two times as first it will be exevuted then fetch will work and will update the vanData then compo will rerender and you will see console two times
 
-    const whatToFetch = searchParam.get('type'); //we are not doing this in useEffect because getting is really quick so we dont need to do it as side effect
-
-    // console.log(whatToFetch);
+    const whatToFetch = searchParams.get('type'); 
+    
+    //we are not doing this in useEffect because getting is really quick so we dont need to do it as side effect
 
     // we can use whattofetch to check whether type is present or not means filter is applied or not, because when no filter no type attribute means whattofetch will be null fslse 
 
@@ -60,33 +60,33 @@ export default function Van() {
 
     //here we memoise the filtered data so that it should not be fetched on each render
 
-    const filtereData = useMemo(() => {
-        return whatToFetch 
-        ? vanData.filter((data) => data.type === whatToFetch) 
-        : vanData;
-    }, [vanData, whatToFetch])
+    // const filtereData = useMemo(() => {
+    //     return whatToFetch 
+    //     ? vanData.filter((data) => data.type === whatToFetch) 
+    //     : vanData;
+    // }, [vanData, whatToFetch])
 
-    const vansList = filtereData.map((data) =>
-    (<div key={data.id} className='w-full md:w-60 p-1'>
-        <Link to={`${data.id}`} state={{search : searchParam.toString(), type : whatToFetch}}>
-        <img className='rounded-md' src={data.imageUrl} alt="Van Image" />
-        </Link>
-        <h2 className='font-semibold mt-1'>{data.name}</h2>
-        <p>{data.price}/day</p>
-        <div className={`flex my-1 items-center justify-center px-4 py-2 rounded-md text-white ${data.type == 'simple' ? 'bg-[#b43333]' : (data.type == 'luxury' ? 'bg-[#0c0702]' : 'bg-[rgb(4,80,35)]')} w-fit`}>{data.type}</div>
-    </div>)
-    );
+    // const vansList = filtereData.map((data) =>
+    // (<div key={data.id} className='w-full md:w-60 p-1'>
+    //     <Link to={`${data.id}`} state={{search : searchParam.toString(), type : whatToFetch}}>
+    //     <img className='rounded-md' src={data.imageUrl} alt="Van Image" />
+    //     </Link>
+    //     <h2 className='font-semibold mt-1'>{data.name}</h2>
+    //     <p>{data.price}/day</p>
+    //     <div className={`flex my-1 items-center justify-center px-4 py-2 rounded-md text-white ${data.type == 'simple' ? 'bg-[#b43333]' : (data.type == 'luxury' ? 'bg-[#0c0702]' : 'bg-[rgb(4,80,35)]')} w-fit`}>{data.type}</div>
+    // </div>)
+    // );
 
-    function genNewSearchParamString(key, value) {
-        const sp = new URLSearchParams(searchParam);
+    // function genNewSearchParamString(key, value) {
+    //     const sp = new URLSearchParams(searchParams);
 
-        if (value === null) {
-            sp.delete(key);
-        } else {
-            sp.set(key, value);
-        }
-        return `?${sp.toString()}`;
-    }
+    //     if (value === null) {
+    //         sp.delete(key);
+    //     } else {
+    //         sp.set(key, value);
+    //     }
+    //     return `?${sp.toString()}`;
+    // }
 
     function handleFilterChange(key, value){
         setSearchParams((prev)=>{
@@ -103,7 +103,7 @@ export default function Van() {
 
     return (
         <div className='p-2 mx-auto w-full'>
-            <nav className='flex md:gap-4 gap-2 px-[10px] my-4'>
+            
                 {/* <NavLink 
                 to='?type=simple' 
                 className= 'active:bg-white active:text-black  rounded-md  py-1 w-20 flex items-center justify-center bg-[#b43333] text-white'
@@ -125,34 +125,67 @@ export default function Van() {
                     className='active:bg-white active:text-black  rounded-md  py-1 w-20 flex items-center justify-center bg-[#b43333] text-white'
                 >Simple</button> */}
 
-                <button
-                    onClick={() => handleFilterChange( "type" ,'simple')}
-                    className={`active:bg-white active:text-black hover:bg-[#b43333]  rounded-md  py-1 w-20 flex items-center justify-center ${whatToFetch === 'simple' ? 'bg-[#b43333]' : 'bg-gray-400'} text-white`}
-                >Simple</button>
-
-                <button onClick={() => handleFilterChange("type" ,"rugged")} className={`active:bg-white active:text-black border-1 rounded-md hover:bg-[rgb(4,80,35)] py-1 w-20 flex items-center justify-center  ${whatToFetch === 'rugged' ? 'bg-[rgb(4,80,35)]' : 'bg-gray-400'} text-white`}>Rugged</button>
-
-                <button onClick={() => handleFilterChange("type" ,"luxury")} className={`active:bg-white active:text-black border-1 rounded-md hover:bg-[#0c0702] py-1 w-20 flex items-center justify-center ${whatToFetch === 'luxury' ? 'bg-[#0c0702]' : 'bg-gray-400' } text-white`}>Luxury</button>
-
-                {whatToFetch ? <button
-                onClick={()=>handleFilterChange("type",null)}
-                className= 'active:bg-white active:text-black  rounded-md  py-1 w-20 flex items-center justify-center bg-[#b43333] text-white'
-                >Clear</button> : null}
-                
                 {/* dont use link or navlink to give onClick event they might not work properly use Button instead */}
 
-                {/* to clear out the url to remove filter we can goive ''  in to of clear button and we can also give '.' because . point the current directory which is van so whole url will change to go to the van page that's our parent page or dir withount any search param*/}
+                {/* to clear out the url to remove filter we can give ''  in to of clear button and we can also give '.' because . point the current directory which is van so whole url will change to go to the van page that's our parent page or dir without any search param*/}
 
                 {/* instead of using to attribute in navlink we can use useSearchParams setter function the data we will give that will directly given to the url  */}
-            </nav>
+                
+                <Suspense fallback={<h1 className='text-2xl font-semibold text-center mt-52'>Loading...</h1>}>
+                <Await resolve={vanDataPromise.vans}>
+                    {
+                        (Data) => {
+                            let vanData = Data.vans
+                            const filtereData = useMemo(() => {
+                                return whatToFetch 
+                                ? vanData.filter((data) => data.type === whatToFetch) 
+                                : vanData || [];
+                            }, [vanData, whatToFetch])
+                        
+                            const vansList = filtereData.map((data) =>
+                            (<div key={data.id} className='w-full md:w-60 p-1'>
+                                <Link to={`${data.id}`} state={{search : searchParams.toString(), type : whatToFetch}}>
+                                <img className='rounded-md' src={data.imageUrl} alt="Van Image" />
+                                </Link>
+                                <h2 className='font-semibold mt-1'>{data.name}</h2>
+                                <p>{data.price}/day</p>
+                                <div className={`flex my-1 items-center justify-center px-4 py-2 rounded-md text-white ${data.type === 'simple' ? 'bg-[#b43333]' : (data.type === 'luxury' ? 'bg-[#0c0702]' : 'bg-[rgb(4,80,35)]')} w-fit`}>{data.type}</div>
+                            </div>)
+                            );
 
-            <div className='flex gap-2 md:gap-3 pb-12 px-[10px] flex-wrap mx-auto  w-full'>
-                {vansList}
-            </div>
+                            return <>
+                                <nav className='flex md:gap-4 gap-2 px-[10px] my-4'>
+                                 <button 
+                                    onClick={() => handleFilterChange( "type" ,'simple')}
+                                    className={`active:bg-white active:text-black hover:bg-[#b43333]  rounded-md  py-1 w-20 flex items-center justify-center ${whatToFetch === 'simple' ? 'bg-[#b43333]' : 'bg-gray-400'} text-white`}
+                                >Simple</button>
+
+                                <button 
+                                    onClick={() => handleFilterChange("type" ,"rugged")} 
+                                    className={`active:bg-white active:text-black border-1 rounded-md hover:bg-[rgb(4,80,35)] py-1 w-20 flex items-center justify-center  ${whatToFetch === 'rugged' ? 'bg-[rgb(4,80,35)]' : 'bg-gray-400'} text-white`}
+                                >Rugged</button>
+
+                                <button 
+                                    onClick={() => handleFilterChange("type" ,"luxury")} 
+                                    className={`active:bg-white active:text-black border-1 rounded-md hover:bg-[#0c0702] py-1 w-20 flex items-center justify-center ${whatToFetch === 'luxury' ? 'bg-[#0c0702]' : 'bg-gray-400' } text-white`}
+                                >Luxury</button>
+
+                                {whatToFetch ? <button
+                                onClick={()=>handleFilterChange("type",null)}
+                                className= 'active:bg-white active:text-black  rounded-md  py-1 w-20 flex items-center justify-center bg-[#b43333] text-white'
+                                >Clear</button> : null}
+                
+                                </nav>
+
+                                <div className='flex gap-2 md:gap-3 pb-12 px-[10px] flex-wrap mx-auto  w-full'>
+                                    {vansList}
+                                </div>
+                            </>
+                        }
+                    }
+                </Await>
+                </Suspense>
         </div>
     )
 
-    // {({isActive})=> `active:bg-white active:text-black ${isActive ? 'border-2 border-black' : ''} rounded-md  py-1 w-20 flex items-center justify-center bg-[#b43333] text-white `}
-
-    // custom isActive, isActive={() => type === 'simple'}
 }
